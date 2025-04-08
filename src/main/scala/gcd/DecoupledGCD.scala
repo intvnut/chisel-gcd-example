e1fae222258110ebb9d8b6a155a05336a2eb7700
Joe Zbiciak <intvnut@gmail.com>
2025-04-07 23:07:36 -0700

    Elimintate dead cycles in GCD input/output
    Add skid buffers on the input and output of GCD, so we can further
    decouple the state machine from the input and output.
    
    This version runs almost twice as fast as the original when there's
    no delays on the input and no backpressure on the output.  It runs
    more than twice as fast as the original when there's delay on the
    input or backpressure on the output, as it overlaps the compute with
    those delays.
    
    Original:
    
    [115] cycles: 650  Delay: in=0 out=0
    [115] cycles: 750  Delay: in=0 out=1
    [115] cycles: 850  Delay: in=0 out=2
    [115] cycles: 749  Delay: in=1 out=0
    [115] cycles: 849  Delay: in=1 out=1
    [115] cycles: 949  Delay: in=1 out=2
    [115] cycles: 848  Delay: in=2 out=0
    [115] cycles: 948  Delay: in=2 out=1
    [115] cycles: 1048  Delay: in=2 out=2
    
    This version:
    
    [115] cycles: 371  Delay: in=0 out=0
    [115] cycles: 399  Delay: in=0 out=1
    [115] cycles: 436  Delay: in=0 out=2
    [115] cycles: 385  Delay: in=1 out=0
    [115] cycles: 400  Delay: in=1 out=1
    [115] cycles: 436  Delay: in=1 out=2
    [115] cycles: 417  Delay: in=2 out=0
    [115] cycles: 419  Delay: in=2 out=1
    [115] cycles: 438  Delay: in=2 out=2

--- src/main/scala/gcd/DecoupledGCD.scala
+++ src/main/scala/gcd/DecoupledGCD.scala
@@ -24,6 +24,103 @@ class GcdOutputBundle(val w: Int) extends Bundle {
   * Can handle stalls on the producer or consumer side
   */
 class DecoupledGcd(width: Int) extends Module {
+  val input       = IO(Flipped(Decoupled(new GcdInputBundle(width))))
+  val output      = IO(Decoupled(new GcdOutputBundle(width)))
+
+  // Input skid buffer.
+  val iBufX    = Reg(UInt())
+  val iBufY    = Reg(UInt())
+  val iBufFull = RegInit(false.B)
+  val iX       = Mux(iBufFull, iBufX, input.bits.value1)
+  val iY       = Mux(iBufFull, iBufY, input.bits.value2)
+
+  // Working registers + GCD compute wires.
+  val origX    = Reg(UInt())
+  val origY    = Reg(UInt())
+  val workX    = Reg(UInt())
+  val workY    = Reg(UInt())
+  val busy     = RegInit(false.B)
+
+  // Output skid buffer.
+  val oBufX    = Reg(UInt())
+  val oBufY    = Reg(UInt())
+  val oBufGcd  = Reg(UInt())
+  val oBufFull = RegInit(false.B)
+
+  // GCD computation step.
+  val diffXY   = workX -& workY
+  val diffYX   = workY -& workX
+  val xIsLess  = diffXY(width)
+  val yIsLess  = diffYX(width)
+                 
+  val xIsZero  = workX === 0.U
+  val xEqualsY = workX === workY
+  val yIsZero  = workY === 0.U
+  val gcdValue = workX | workY
+  val gcdValid = busy && (xIsZero || xEqualsY || yIsZero)
+
+  // State transition signals.
+  val oAvail   = !oBufFull
+  val oAccept  = gcdValid && oAvail
+  val iValid   = input.valid || iBufFull
+  val iAccept  = oAccept || !busy
+  val iAdvance = iAccept || (input.valid && !iBufFull) // Advance iBuf.
+
+  // Input buffer updates for next cycle.
+  input.ready := iAccept || !iBufFull                  // Dequeue input.
+  when (iAdvance) {
+    iBufX    := input.bits.value1
+    iBufY    := input.bits.value2
+    iBufFull := Mux(input.valid === iAccept, iBufFull, input.valid)
+  }
+
+  // Update working registers based on our computed state transition.
+  when (busy) {
+    when (iValid && iAccept) {
+      origX := iX
+      origY := iY
+      workX := iX
+      workY := iY
+      busy  := true.B
+    } .otherwise {
+      // This formulation does not go to [0, 0] when X === Y, which allows
+      // us to read the GCD off of X | Y.
+      workX := Mux(yIsLess, diffXY(width - 1, 0), workX)
+      workY := Mux(xIsLess, diffYX(width - 1, 0), workY)
+      busy  := !gcdValid || !oAvail
+    }
+  } .elsewhen (iValid) {
+    origX := iX
+    origY := iY
+    workX := iX
+    workY := iY
+    busy  := true.B
+  }
+
+  // Output buffer updates for next cycle
+  output.bits.value1 := Mux(oBufFull, oBufX,   origX)
+  output.bits.value2 := Mux(oBufFull, oBufY,   origY)
+  output.bits.gcd    := Mux(oBufFull, oBufGcd, gcdValue)
+  output.valid       := oBufFull || gcdValid
+
+  when (oAccept) {
+    oBufX    := origX
+    oBufY    := origY
+    oBufGcd  := gcdValue
+    oBufFull := (gcdValid && !output.ready) || oBufFull
+  } .elsewhen (output.ready) {
+    oBufFull := false.B
+  }
+}
+
+/**
+  * Compute Gcd using subtraction method.
+  * Subtracts the smaller from the larger until register y is zero.
+  * value input register x is then the Gcd.
+  * Unless first input is zero then the Gcd is y.
+  * Can handle stalls on the producer or consumer side
+  */
+class DecoupledGcdOrig(width: Int) extends Module {
   val input = IO(Flipped(Decoupled(new GcdInputBundle(width))))
   val output = IO(Decoupled(new GcdOutputBundle(width)))
 
--- src/test/scala/gcd/GCDSpec.scala
+++ src/test/scala/gcd/GCDSpec.scala
@@ -72,11 +72,17 @@ import DefaultSimulator._
 class GCDSpec extends AnyFreeSpec with Matchers {
 
   "Gcd should calculate proper greatest common denominator" in {
-    simulate(new DecoupledGcd(16), buildDir = "build", enableWaves = true) { dut =>
+    simulate(new DecoupledGcd(16), buildDir = "build", enableWaves = true) {
+      dut =>
       val testValues = for { x <- 0 to 10; y <- 0 to 10} yield (x, y)
-      val inputSeq = testValues.map { case (x, y) => (new GcdInputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U) }
-      val resultSeq = testValues.map { case (x, y) =>
-        (new GcdOutputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U, _.gcd -> BigInt(x).gcd(BigInt(y)).U)
+      val inputSeq = testValues.map {
+        case (x, y) => (new GcdInputBundle(16)).
+                        Lit(_.value1 -> x.U, _.value2 -> y.U)
+      }
+      val resultSeq = testValues.map {
+        case (x, y) => (new GcdOutputBundle(16)).
+                        Lit(_.value1 -> x.U, _.value2 -> y.U,
+                        _.gcd -> BigInt(x).gcd(BigInt(y)).U)
       }
 
       dut.reset.poke(true.B)
@@ -84,30 +90,57 @@ class GCDSpec extends AnyFreeSpec with Matchers {
       dut.reset.poke(false.B)
       dut.clock.step()
 
-      var sent, received, cycles: Int = 0
-      while (sent != 100 && received != 100) {
-        assert(cycles <= 1000, "timeout reached")
+      for (inDelay <- 0 to 2) {
+        for (outDelay <- 0 to 2) {
+          var sent, received, cycles, delayIn, delayOut: Int = 0
+          while (sent != 100 || received != 100) {
+            assert(cycles <= 1500, "timeout reached")
 
-        if (sent < 100) {
-          dut.input.valid.poke(true.B)
-          dut.input.bits.value1.poke(testValues(sent)._1.U)
-          dut.input.bits.value2.poke(testValues(sent)._2.U)
-          if (dut.input.ready.peek().litToBoolean) {
-            sent += 1
+//println(s"dIn=$delayIn dOut=$delayOut sent=$sent recv=$received")
+
+            if (sent < 100 && delayIn == 0) {
+              dut.input.valid.poke(true.B)
+              dut.input.bits.value1.poke(testValues(sent)._1.U)
+              dut.input.bits.value2.poke(testValues(sent)._2.U)
+              if (dut.input.ready.peek().litToBoolean) {
+//println(s"send: ${testValues(sent)._1} ${testValues(sent)._2}")
+                sent += 1
+              } else {
+//println("not ready")
+              }
+              delayIn = inDelay
+            } else {
+              dut.input.valid.poke(false.B)
+              if (delayIn > 0 && dut.input.ready.peek().litToBoolean) {
+                delayIn = delayIn - 1
+              }
+            }
+
+            if (received < 100 && delayOut == 0) {
+              dut.output.ready.poke(true.B)
+              if (dut.output.valid.peek().litToBoolean) {
+//println(s"recv: ${dut.output.bits.value1.peek().litValue} ${dut.output.bits.value2.peek().litValue} ${dut.output.bits.gcd.peek().litValue}")
+                dut.output.bits.value1.expect(testValues(received)._1)
+                dut.output.bits.value2.expect(testValues(received)._2)
+                dut.output.bits.gcd.expect(
+                  BigInt(testValues(received)._1).gcd(testValues(received)._2)
+                )
+                received += 1
+              }
+              delayOut = outDelay
+            } else {
+              dut.output.ready.poke(false.B)
+              if (delayOut > 0 && dut.output.valid.peek().litToBoolean) {
+                delayOut = delayOut - 1
+              }
+            }
+
+            // Step the simulation forward.
+            dut.clock.step()
+            cycles += 1
           }
+          println(s"cycles: $cycles  Delay: in=$inDelay out=$outDelay")
         }
-
-        if (received < 100) {
-          dut.output.ready.poke(true.B)
-          if (dut.output.valid.peekValue().asBigInt == 1) {
-            dut.output.bits.gcd.expect(BigInt(testValues(received)._1).gcd(testValues(received)._2))
-            received += 1
-          }
-        }
-
-        // Step the simulation forward.
-        dut.clock.step()
-        cycles += 1
       }
     }
   }
